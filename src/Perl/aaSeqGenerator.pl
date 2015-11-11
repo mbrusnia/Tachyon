5008cdc5d25bc67ee001f964f7139bba123587e6
Hector <ramoshf@gmail.com>
2015-11-11 08:57:46 -0800

    Update aaSeqGenerator.pl

--- src/Perl/aaSeqGenerator.pl
+++ src/Perl/aaSeqGenerator.pl
@@ -10,7 +10,6 @@ for($b = 0; $b< $naas; $b++){
 for($c = 0; $c< $naas; $c++){
 for($d = 0; $d< $naas; $d++){
 	$seq = $AA[$a] . $AA[$b] . $AA[$c] . $AA[$d] . "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-	#print $i++ . "\t" . $seq ."\n";
-	unless ($seq =~ m/[ACDEFGHIKLMNPQRSTVWY]+/){print "wtf\n";}
+	print $i++ . "\t" . $seq ."\n";
 	if($i > 10000){exit;}
 }}}}
