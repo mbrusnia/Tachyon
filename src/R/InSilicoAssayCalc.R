7ddf41631f4ee4f62e9878388da29803c7dbb56e
Hector <hector@integrativews.com>
2016-01-28 17:20:11 -0800

    Skips duplicate sequences
    Does not do the calculations for sequences which it has done it before.

--- src/R/InSilicoAssayCalc.R
+++ src/R/InSilicoAssayCalc.R
@@ -9,9 +9,11 @@ library(Peptides)
 
 
 
-#Parameters for this script
-BASE_URL = "http://localhost:8080/labkey"
-FOLDER_PATH = "/Optides 01"
+#Parameters for this script (login script: _netrc)
+BASE_URL = "http://optides-stage.fhcrc.org/"
+FOLDER_PATH = "/Optides"
+#BASE_URL = "http://localhost:8080/labkey"
+#FOLDER_PATH = "/Optides 01"
 SCHEMA_NAME = "Samples"
 QUERY_NAME  = "IdentifiedCompounds"
 COMPOUNDID_COLNAME = "CompoundID"
@@ -32,16 +34,21 @@ mydata <- labkey.selectRows(
 	containerFilter=NULL
 )
 
+#remove compounds for which we have already done the calculation
+previousAssayResults <- labkey.selectRows(BASE_URL, FOLDER_PATH, paste("assay.General.", DESTINATION_ASSAY_NAME, sep=""), "Data", viewName = NULL, colSelect = c(COMPOUNDID_COLNAME , SEQUENCE_COLNAME ), maxRows = NULL, rowOffset = NULL, colSort = NULL, colFilter = NULL, showHidden = FALSE)
 
+#calculate desired values
 avg = vector('numeric')
 monoisotopic = vector('numeric')
 pIs = vector('numeric')
 
-for(i in 1:length(Sequence)){
+for(i in 1:length(mydata$Sequence)){
 	#cat(Sequence[i], "   ", mw(mydata$Sequence[i], monoisotopic=TRUE), "\n")
-	avg[i] = mw(mydata$Sequence[i], monoisotopic=FALSE)
-	monoisotopic[i] = mw(mydata$Sequence[i], monoisotopic=TRUE)
-	pIs[i] = pI(mydata$Sequence[i], pKscale="Sillero")
+	if(!mydata$Sequence %in% previousAssayResults$Sequence){
+		avg[i] = mw(mydata$Sequence[i], monoisotopic=FALSE)
+		monoisotopic[i] = mw(mydata$Sequence[i], monoisotopic=TRUE)
+		pIs[i] = pI(mydata$Sequence[i], pKscale="Sillero")
+	}
 }
 
 mydata$MonoisotopicMass = monoisotopic
@@ -49,7 +56,9 @@ mydata$AverageMass = avg
 mydata$pI = pIs
 
 #insert results as a batch into our destination assay
-labkey.saveBatch(BASE_URL, DESTINATION_FOLDER_PATH, DESTINATION_ASSAY_NAME, mydata,
-batchPropertyList=NULL, runPropertyList=NULL)
+if(length(monoisotopic) > 0){
+	labkey.saveBatch(BASE_URL, DESTINATION_FOLDER_PATH, DESTINATION_ASSAY_NAME, mydata,
+	batchPropertyList=NULL, runPropertyList=NULL)
+}
 
 
