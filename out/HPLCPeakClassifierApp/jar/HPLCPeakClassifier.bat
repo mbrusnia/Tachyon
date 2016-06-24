7fbcff23705f49e2da217c6998fb02bbab88d092
Hector <hector@integrativews.com>
2016-06-23 17:01:19 -0700

    HPLCPeakClassifier Ant build and execution script commit

---
+++ out/HPLCPeakClassifierApp/jar/HPLCPeakClassifier.bat
@@ -0,0 +1,9 @@
+java -jar HPLCPeakClassifier.jar ^
+--BLANK_R="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/004-2201.D/BLANK_H2O_4_DAD.CSV" ^
+--BLANK_NR="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/004-2201.D/BLANK_H2O_4_DAD2.CSV" ^
+--NR="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/1BG-2301.D/HTP013_B4_NR_DAD.CSV" ^
+--R="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/1BH-2401.D/HTP013_B4_DTT_DAD.CSV" ^
+--sampleInfo="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/1BG-2301.D/SAMPLE.XML" ^
+--outdir="C:/Users/hramos/Documents/HRInternetConsulting/Clients/FHCRC/Project17 - HPLCPeakClassifierApp/" ^
+--SN=.35 ^
+--Classification=3
