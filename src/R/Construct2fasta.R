d2a5a568a852843127c3c3d861aec24a58d1c125
Hector <hector@integrativews.com>
2016-09-06 14:17:10 -0700

    Construct2Fasta.R initial commit

---
+++ src/R/Construct2fasta.R
@@ -0,0 +1,26 @@
+#This script will read data from optides-prod.fhcrc.org server and write
+#it out as a fasta file.  Specifically, it will take data from the Construct
+#sampleSet in the CompoundsRegistry folder and write the fasta file to the 
+#working directory.
+
+
+library(Rlabkey)
+options(stringsAsFactors = FALSE)
+
+mydata <- labkey.selectRows(
+	baseUrl="http://optides-prod.fhcrc.org",
+	folderPath="/Optides/CompoundsRegistry/Samples",
+	schemaName="samples",
+	queryName="Construct",
+	colSelect=c("ID", "ParentID", "AlternateName", "AASeq"),
+	colNameOpt="fieldname"
+)
+
+filename <-paste0("OptideConstruct_", Sys.Date(), ".fasta")
+sink(filename)
+for(i in 1:length(mydata$ID)){
+	cat(paste0(">", mydata$ID[i], "\t", mydata$ParentID[i], "\t", mydata$AlternateName[i], "\n", mydata$AASeq[i], "\n"))
+}
+sink()
+
+
