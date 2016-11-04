e85fc6c6f695e0024bc9736600b7e68ab3ca8564
Hector <hector@integrativews.com>
2016-11-04 15:06:17 -0700

    Look up StandardCurve info from a List

--- src/R/LSCActivityCalc.R
+++ src/R/LSCActivityCalc.R
@@ -1,6 +1,7 @@
 options(stringsAsFactors = FALSE)
+library(Rlabkey)
 
-baseUrl<-"http://optides-prod.fhcrc.org/"
+BASE_URL <- "http://optides-prod.fhcrc.org/"
 ${rLabkeySessionId}
 rpPath<- "${runInfo}"
 
@@ -10,7 +11,6 @@ getRunPropsList<- function(rpPath, baseUrl) {
 		stringsAsFactors=FALSE, sep="\t", quote="", fill=TRUE, na.strings="");       ######### 
 
 	## pull out the run properties
-
 	params<- list(inputPathUploadedFile = rpIn$val1[rpIn$name=="runDataUploadedFile"],
 		inputPathValidated = rpIn$val1[rpIn$name=="runDataFile"],
 		
@@ -21,14 +21,14 @@ getRunPropsList<- function(rpPath, baseUrl) {
 		runPropsOutputPath = rpIn$val1[rpIn$name=="transformedRunPropertiesFile"],
 		sampleSetId = as.integer(rpIn$val1[rpIn$name=="sampleSet"]),
 		probeSourceId = as.integer(rpIn$val1[rpIn$name=="probeSource"]),
-		calibrationFactor = as.numeric(rpIn$val1[rpIn$name=="StandardCurve"]),
+		standardCurve = rpIn$val1[rpIn$name=="StandardCurve"],
 		errorsFile = rpIn$val1[rpIn$name=="errorsFile"])
 	return (params)
 
 }
 
 ## read the file paths etc out of the runProperties.tsv file
-params <- getRunPropsList(rpPath, baseUrl)
+params <- getRunPropsList(rpPath, BASE_URL)
 
 ## read the input data frame. Is xlsx or tsv?
 if(tools::file_ext(params$inputPathUploadedFile) == "xlsx"){
@@ -47,18 +47,25 @@ if(grepl("MouseID", names(inputDF)[1]) && grepl("CompoundID", names(inputDF)[2])
 	&& grepl("Tissue", names(inputDF)[3]) && grepl("Date", names(inputDF)[4])
 	&& grepl("Tissue_mg", names(inputDF)[5]) && grepl("mg_per_ul", names(inputDF)[6])
 	&& grepl("Loaded_Volume_uL", names(inputDF)[7]) && grepl("CPM", names(inputDF)[8])
-	&& grepl("Loaded_mg", names(inputDF)[9]) && grepl("pCi", names(inputDF)[10])
+	&& grepl("Loading_mg", names(inputDF)[9]) && grepl("pCi", names(inputDF)[10])
 	&& grepl("pCi_per_uL", names(inputDF)[11])	){	
 	1==1
 }else{
 	stop("This file does not conform to the expected format.  These are the expected column headers (in this order): MouseID	CompoundID	Tissue	Date	Tissue_mg	mg_per_ul	Loaded_Volume_uL	CPM	Loaded_mg	pCi	pCi_per_uL")
 }
 
-#names(inputDF) = c("PlateID", "WellID", "Specimen", "Sample", "M3_Percent_Parent", "M3_Median_FITC_H", "P1_percent_All", "R2_percent_Parent", "Run_Time", "R2_Abs_Count")
-inputDF$pCi <- params$calibrationFactor * as.numeric(inputDF$CPM)
-inputDF$Loaded_mg <- as.numeric(inputDF$Loaded_Volume_uL) * as.numeric(inputDF$mg_per_ul)
-inputDF$pCi_per_uL <- as.numeric(inputDF$pCi) / as.numeric(inputDF$Loaded_Volume_uL)
+#
+#First step get AverageMW value from ChemProductionID = MW
+standardsList <- labkey.selectRows(
+    baseUrl=BASE_URL,
+    folderPath="/Optides/VIVOAssay/Sample",
+    schemaName="lists",
+    queryName="LSCpCiConversionFactor"
+)
 
+inputDF$pCi <- as.numeric(standardsList[standardsList[,"Version"] == params$standardCurve, "Slope"]) * as.numeric(inputDF$CPM) + as.numeric(standardsList[standardsList[,"Version"] == params$standardCurve, "YIntercept"])
+inputDF$Loading_mg <- as.numeric(inputDF$Loaded_Volume_uL) * as.numeric(inputDF$mg_per_ul)
+inputDF$pCi_per_uL <- as.numeric(inputDF$pCi) / as.numeric(inputDF$Loaded_Volume_uL)
 
 ###################################################################
 ## 2) Insert data to Database
