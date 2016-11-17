0f31130b89bf9dd677ffe0bb8b0517eb6a87b565
mbrusnia <mbrusnia@fhcrc.org>
2016-11-17 15:16:17 -0800

    Add new column HydrophobicityAtpH7_5

--- src/labkey_schemas/parameterizedInSilicoAssayQuery.sql
+++ src/labkey_schemas/parameterizedInSilicoAssayQuery.sql
@@ -3,6 +3,6 @@ PARAMETERS
     constructIDsLookupKey VARCHAR(32)
 )
 
-SELECT ID, ParentID, AlternateName, AASeq, Vector, AverageMass, MonoisotopicMass, ReducedForm_pI, NetChargeAtpH7_4
+SELECT ID, ParentID, AlternateName, AASeq, Vector, AverageMass, MonoisotopicMass, ReducedForm_pI, NetChargeAtpH7_4, HydrophobicityAtpH7_5
 FROM "/Optides/InSilicoAssay/MolecularProperties/".assay.General.InSilicoAssay.Data
 WHERE ID in (select ConstructID from lists.constructQueryLookupTable where LookupKey=constructIDsLookupKey)
