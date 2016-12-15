a504e7b836f34be0b9385de0153cd4ab3b6215ed
Hector <hector@integrativews.com>
2016-12-15 14:12:02 -0800

    now includes Vector in check for Construct Duplicates

--- src/labkey_schemas/AASequenceDuplicates.sql
+++ src/labkey_schemas/AASequenceDuplicates.sql
@@ -3,21 +3,18 @@ from "/Optides/CompoundsRegistry/Samples/".samples.Construct
 where AASeq in(
    select AASeq
    from (
-         SELECT AASeq 
-         from "/Optides/CompoundsRegistry/Samples/".samples.Construct
-	)
-	group by AASeq
-	having count(*) > 1 
+         SELECT AASeq, Vector
+		FROM "/Optides/CompoundsRegistry/Samples/".samples.Construct table1
+		group by AASeq, Vector
+      	having count(*) > 1
+		) t1
 )
 Union All
 select ID, AASeq
 from "/Optides/CompoundsRegistry/Samples/".samples.Variant
 where AASeq in(
    select AASeq
-   from (
-         SELECT AASeq 
-         from "/Optides/CompoundsRegistry/Samples/".samples.Variant
-	)
+   from "/Optides/CompoundsRegistry/Samples/".samples.Variant
 	group by AASeq
 	having count(*) > 1 
 )
@@ -26,10 +23,7 @@ select ID, AASeq
 from "/Optides/CompoundsRegistry/Samples/".samples.Homologue
 where AASeq in(
    select AASeq
-   from (
-         SELECT AASeq 
-         from "/Optides/CompoundsRegistry/Samples/".samples.Homologue
-	)
+   from "/Optides/CompoundsRegistry/Samples/".samples.Homologue
 	group by AASeq
-	having count(*) > 1 
+	having count(*) > 1
 )
