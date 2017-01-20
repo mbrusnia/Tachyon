31cde91ab14195a89d1cb118c90039ef53bdaead
mbrusnia <mbrusnia@fhcrc.org>
2017-01-19 16:30:32 -0800

    Due to update in desgin in production, the code is updated accordingly.

--- src/labkey_schemas/otdReport.samples.parameterizedOTDNovocyte.sql
+++ src/labkey_schemas/otdReport.samples.parameterizedOTDNovocyte.sql
@@ -3,6 +3,6 @@ PARAMETERS
     otdIDsLookupKey VARCHAR(32)
 
 )
-SELECT Sample, Specimen, M3_Percent_Parent as "M3 % Parent", M3_Median_FITC_H as "M3 Median FITC-H", P1_percent_All as "P1 % All", R2_percent_Parent as "R2 % Parent", Run_Time, R2_Abs_Count as "R2 Abs Count"
+SELECT Sample, percent_GFP as "% GFP", Median_GFP as "Median GFP", FSC_over_SSC as "FSC/SSC", percent_Viable as "% Viable", Abs_Count as "Absolute Count"
 FROM "/Optides/OTDProduction/Assays/".assay.General.Novocyte_TransductionReport.Data
 WHERE Sample in (select OTDID from lists.otdQueryLookupTable where LookupKey=otdIDsLookupKey)
