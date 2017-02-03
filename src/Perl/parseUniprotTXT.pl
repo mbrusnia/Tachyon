ad338cce4cd445a1300729cea903797f72468915
Hector <ramoshf@gmail.com>
2017-02-03 15:38:52 -0800

    initial Commit

---
+++ src/Perl/parseUniprotTXT.pl
@@ -0,0 +1,56 @@
+use LWP::Simple;
+
+open( my $fh, '<', "C:/Users/Hector/Documents/HRInternetConsulting/Clients/FHCRC/Project25 - Research/Project25_ProteinList.txt" ) or die "Can't open $filename: $!";
+my $i = 0;
+    while ( my $protID = <$fh>) {
+		chomp $protID;
+		$content = get("http://www.uniprot.org/uniprot/" . $protID . ".txt\n");
+			die "Couldn't get it!" unless defined $content;
+		
+		my $id;
+		my $chain = "false";
+		my $chainId;
+		my $signalPep = "false";
+		my $isSeq = "false";
+		my $seq;
+		@a = split(/\n/, $content);
+		for ($j=0; $j < scalar(@a); $j++){
+			my $ln = $a[$j];
+			#<feature type="signal peptide"> 
+			#<feature type="chain"> with id starting with “PRO”,
+			if ( $ln =~ /^FT   SIGNAL/) {
+				$signalPep = "true";
+			}elsif ( $ln =~ /^FT   CHAIN/) {
+				$j++;
+				if($a[$j] =~ /FTId=PRO/){
+					$chain = "true";
+				}
+			}elsif($ln =~ /SQ   SEQUENCE/){
+				$isSeq = "true";
+			}elsif($isSeq eq "true"){
+				if($ln =~/^\/\//){
+					$isSeq = "false";
+				}else{
+					$seq .=  join('', split(/\W+/, $ln))
+				}
+			}
+		}
+		
+		if($chain eq "true" or $signalPep eq "true"){
+			#print "---YES-------\n" . "http://www.uniprot.org/uniprot/" . $protID . ".txt\n";
+			print ">" . $protID . ":";
+			if($signalPep eq "true"){
+				print "signal peptide";
+			}
+			if($signalPep eq "true" and $chain eq "true"){
+				print ",";
+			}
+			if($chain eq "true"){
+				print "PRO";
+			}
+			print "\n" . $seq . "\n";
+		} #else{
+			print "---NO--------\n" . "http://www.uniprot.org/uniprot/" . $protID . ".txt\n\n";
+		#}
+    }
+close $fh;
