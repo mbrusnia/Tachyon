78819de0e14212538cf3bef512e23dfe043e4d9f
Hector <hector@integrativews.com>
2017-04-18 17:17:38 -0700

    working script

--- src/labkey_modules/CompoundsRegistryNewSequenceInsert_pipeline/pipeline/tasks/insert_chemical_product.R
+++ src/labkey_modules/CompoundsRegistryNewSequenceInsert_pipeline/pipeline/tasks/insert_chemical_product.R
@@ -25,6 +25,11 @@ jobInfo <- read.table(jobInfoFile,
 inputFile <- "${input.xlsx}"
 inputDF <- xlsxToR(inputFile, header=TRUE)
 
+#take the spaces out of the column headers
+for(i in 1:length(names(inputDF))){
+	names(inputDF)[i] = gsub(" ", "", names(inputDF)[i])
+}
+
 ##
 ##Parameters for this script (login script: _netrc)
 ##
@@ -33,9 +38,9 @@ CONTAINER_PATH <- jobInfo$value[ grep("containerPath", jobInfo$name)]
 SAMPLE_SETS_SCHEMA_NAME <- "samples"
 SAMPLESET_NAME <- "CHEMProduction"
 ID_COL_NAME <- "CHEMProductionID"
-CONJUGATION_METHOD_COL_NAME = "Conjugation Method"
-OTDPRODUCTIONID_COL_NAME = "OTDProduction ID"
-AVG_MOL_WEIGHT_COL_NAME = "Average MW"
+CONJUGATION_METHOD_COL_NAME = "ConjugationMethod"
+OTDPRODUCTIONID_COL_NAME = "OTDProductionID"
+AVG_MOL_WEIGHT_COL_NAME = "AverageMW"
 
 OTDPRODUCTION_SAMPESET_NAME = "OTDProduction"
 SEQUENCE_COL_NAME <- "AASeq"
@@ -78,8 +83,6 @@ if(length(previousChemProductionContents[,ID_COL_NAME]) > 0){
 ##
 inputDF[, ID_COL_NAME] <- ""
 
-#cat("Inserting ", length(inputDF[,ID_COL_NAME]), " new sequences.  New IDs begin with ", paste0("CNT", str_pad(newID, 7, "0", side="left")), " and end with ", paste0("CNT", str_pad(newID + length(inputDF[,ID_COL_NAME]) - 1, 7, "0", side="left")), "\n")
-
 for(i in 1:length(inputDF[,ID_COL_NAME])){
 	if(inputDF[i, CONJUGATION_METHOD_COL_NAME] == "C14 reductive amination"){
 		inputDF[i, ID_COL_NAME] <- paste0("CHH", str_pad(nextIDnum, 7, "0", side="left"))
@@ -113,7 +116,7 @@ for(i in 1:length(inputDF[,ID_COL_NAME])){
 		sequence <- labkey.selectRows(BASE_URL, CONTAINER_PATH, 
 			SAMPLE_SETS_SCHEMA_NAME, "Construct", colSelect = c("ID", "AASeq"), 
 			colFilter=makeFilter(c("ID", "EQUAL", constructID)), colNameOpt="fieldname")$AASeq[1]
-#inputDF$sequence[i] = sequence
+		#inputDF$sequence[i] = sequence
 		#calculate Molecular Weight
 		inputDF[i, AVG_MOL_WEIGHT_COL_NAME] = DSBMWCalc(sequence, TRUE) + (str_count(sequence, "K")+1) * 2.0 * (calc_formula_mass("C1H2")+ DeltaC14)
 	
@@ -125,30 +128,24 @@ for(i in 1:length(inputDF[,ID_COL_NAME])){
 	}
 }
 
-
-
-#mydata <- labkey.selectRows(
-#    baseUrl="https://optides-prod.fhcrc.org",
-#    folderPath="/Optides/CompoundsRegistry/Samples",
-#    schemaName="samples",
-#    queryName="CHEMProduction",
-#    colSelect = c("CHEMProductionID",  "ConjugationMethod", "OTDProductionID",   "AverageMW"),
-#    colNameOpt="fieldname"
-#)
 ##
 ##insert into DB
 ##
-##ssi <- labkey.insertRows(
-##	baseUrl=BASE_URL,
-##	folderPath=CONTAINER_PATH,
-##	schemaName=SAMPLE_SETS_SCHEMA_NAME,
-##	queryName="CHEMProduction",
-##	toInsert=inputDF
-##)
+ssi <- labkey.insertRows(
+	baseUrl=BASE_URL,
+	folderPath=CONTAINER_PATH,
+	schemaName=SAMPLE_SETS_SCHEMA_NAME,
+	queryName=SAMPLESET_NAME,
+	toInsert=inputDF
+)
 
 if(!exists("ssi")){
 	stop("The insertion into the database failed.  Please contact the administrator.")
 }else{
 	#completed
-	cat(length(inputDF$AASeq), " RECORDS HAVE BEEN INSERTED.\n")
+	cat(length(inputDF[,ID_COL_NAME]), " RECORDS HAVE BEEN INSERTED.  These are their CHEMProductionID's:\n")
+	for(i in 1:length(inputDF[,ID_COL_NAME])){
+		cat(inputDF[i, ID_COL_NAME], " ")
+	}
+	cat("\n")
 }
