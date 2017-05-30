deb1f09db14eeab5d58fcd460fc99224b07a1be7
Hector <hector@integrativews.com>
2017-05-29 19:32:04 -0700

    now updates OTDProductionReport's MonoisotopicMass column too

--- src/R/updateMWs.R
+++ src/R/updateMWs.R
@@ -1,3 +1,7 @@
+##
+## This script updated the molecular weights in /CompoundsRegistry/Samples CHEMProduction
+## and OTDProduction/Assays/ OTDProductionReport
+##
 options(stringsAsFactors = FALSE)
 library(Rlabkey)
 library(stringr)
@@ -31,7 +35,7 @@ for(i in 1:length(chemProd$OTDProductionID)){
 		constructIDs <- labkey.selectRows(BASE_URL, CONTAINER_PATH, 
 			SAMPLE_SETS_SCHEMA_NAME, "OTDProduction", colSelect = c("OTDProductionID", "ParentID"), 
 			colFilter=makeFilter(c("OTDProductionID", "EQUAL", otdProdID)), colNameOpt="fieldname")
-		if(length(constructIDs) < 1){
+		if(length(constructIDs$OTDProductionID) < 1){
 			stop(paste0("The OTDProductionID: ", otdProdID, " is not found in the OTDProduction Sampleset!  Please correct this issue and try again."))
 		}
 		constructID = gsub("Construct.", "", constructIDs$ParentID[1])
@@ -42,7 +46,7 @@ for(i in 1:length(chemProd$OTDProductionID)){
 			colFilter=makeFilter(c("ID", "EQUAL", constructID)), colNameOpt="fieldname")$AASeq[1]
 		#inputDF$sequence[i] = sequence
 		#calculate Molecular Weight
-		chemProd$NewAverageMW[i] = DSBMWCalc(sequence) + (str_count(sequence, "K")+1) * 2.0 * (calc_formula_mass("C1H2")+ DeltaC14)
+		chemProd$NewAverageMW[i] = round(DSBMWCalc(sequence) + (str_count(sequence, "K")+1) * 2.0 * (calc_formula_mass("C1H2")+ DeltaC14), digit=2)
 	
 	#else, the value given in the input file is fine
 	}else{
@@ -67,8 +71,74 @@ if(!exists("ssi")){
 	#completed
 	cat(length(chemProd$CHEMProductionID), " RECORDS HAVE BEEN UPDATED.  These are their CHEMProductionID's:\n")
 	for(i in 1:length(chemProd$CHEMProductionID)){
-		cat(chemProd$CHEMProductionID[i], " ")
+		cat(chemProd$CHEMProductionID[i], "\n")
 	}
-	cat("\n")
+
 }
+
+
+
+######
+
+CONTAINER_PATH = "/Optides/OTDProduction/Assays"
+SAMPLE_SETS_SCHEMA_NAME = "samples"
+
+otdProdReport <- labkey.selectRows(
+    baseUrl=BASE_URL,
+    folderPath=CONTAINER_PATH,
+    schemaName=SAMPLE_SETS_SCHEMA_NAME,
+    queryName="OTDProductionReport",
+	colNameOpt="fieldname", 
+	colFilter=makeFilter(c("OTDProductionID", "NOT_MISSING", "")),  
+    colSelect=c("RowId", "OTDProductionID", "MonoisotopicMass")
+)
+
+for(i in 1:length(otdProdReport$OTDProductionID)){
+	#get sequence in order to calculate weight
+	otdProdID = otdProdReport$OTDProductionID[i]
+		
+	#get ConstructID
+	constructIDs <- labkey.selectRows(BASE_URL, "/Optides/CompoundsRegistry/Samples", 
+		SAMPLE_SETS_SCHEMA_NAME, "OTDProduction", colSelect = c("OTDProductionID", "ParentID"), 
+		colFilter=makeFilter(c("OTDProductionID", "EQUAL", otdProdID)), colNameOpt="fieldname")
+	if(length(constructIDs$OTDProductionID) < 1){
+		next
+		#stop(paste0("The OTDProductionID: ", otdProdID, " is not found in the OTDProduction Sampleset!  Please correct this issue and try again."))
+	}
+	constructID = gsub("Construct.", "", constructIDs$ParentID[1])
+
+	#get sequence
+	sequence <- labkey.selectRows(BASE_URL, "/Optides/CompoundsRegistry/Samples", 
+		SAMPLE_SETS_SCHEMA_NAME, "Construct", colSelect = c("ID", "AASeq"), 
+		colFilter=makeFilter(c("ID", "EQUAL", constructID)), colNameOpt="fieldname")$AASeq[1]
+	#inputDF$sequence[i] = sequence
+	#calculate Molecular Weight
+	otdProdReport$MonoisotopicMass[i] = round(DSBMWCalc(sequence, monoisotopic=TRUE) + (str_count(sequence, "K")+1) * 2.0 * (calc_formula_mass("C1H2", monoisotopic=TRUE)+ DeltaC14), digit=2)
+}
+
+##
+##insert into DB
+##
+ssi2 <- labkey.updateRows(
+	baseUrl=BASE_URL,
+	folderPath=CONTAINER_PATH,
+	schemaName=SAMPLE_SETS_SCHEMA_NAME,
+	queryName="OTDProductionReport",
+	toUpdate=otdProdReport
+)
+
+if(!exists("ssi2")){
+	stop("The insertion into the database failed.  Please contact the administrator.")
+}else{
+	#completed
+	cat(length(otdProdReport$OTDProductionID), " RECORDS HAVE BEEN UPDATED.  These are their OTDProductionID's:\n")
+	for(i in 1:length(otdProdReport$OTDProductionID)){
+		if(!is.na(otdProdReport$MonoisotopicMass[i])){
+			cat(otdProdReport$OTDProductionID[i], "\n")
+		}else
+			cat(otdProdReport$OTDProductionID[i], "'s MonoisotopicMass could not be updated properly.", "\n")
+	}
+
+}
+
 
