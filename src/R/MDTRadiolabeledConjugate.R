ccf82f0ed6391c41a97dbf3bf06dd07e17cc5ab5
mbrusnia <mbrusnia@fhcrc.org>
2019-04-04 12:28:05 -0700

    Sometimes Excel input file from Liz has ill formatted 10-6 digit. I round it up to 2 decimal points.

--- src/R/MDTRadiolabeledConjugate.R
+++ src/R/MDTRadiolabeledConjugate.R
@@ -12,7 +12,6 @@ rpPath<- "${runInfo}"
 
 ## read the file paths etc out of the runProperties.tsv file
 params <- getRunPropsList(rpPath)
-
 ## read the input data frame. Is xlsx or csv?
 if(tools::file_ext(params$inputPathUploadedFile) == "xlsx"){
 	source("${srcDirectory}/xlsxToR.R")
@@ -26,13 +25,17 @@ ELUTE_CPM = which(names(inputDF) == "Elute_CPM");
 ELUTE_ML = which (names(inputDF) == "Elute_mL");
 ndata = dim(inputDF)[1]
 SPECIFIC_ACTIVITY = vector(mode="numeric", length=ndata)
+
 # The constant is built from experimental protocol like 95% of the volumn, 100x dilution etc.
 # It also uses CPM to Ci conversion factor 2220000000000 and unit factor uCi 0.000001
 # ((((1/0.95)/2220000000000)*100*1)/0.000001) = 0.0000474158368895
 CONVERSION_CONST = 0.0000474158368895
 for(i in 1:ndata){
-    SPECIFIC_ACTIVITY[i] = round(as.numeric(inputDF[i,ELUTE_CPM])/as.numeric(inputDF[i,INPUT_PROTEIN_MG])*as.numeric(inputDF[i,ELUTE_ML])*100.0*CONVERSION_CONST, digit=2)
+    SPECIFIC_ACTIVITY[i] = round(as.numeric(inputDF[i,ELUTE_CPM])/as.numeric(inputDF[i,INPUT_PROTEIN_MG])*as.numeric(inputDF[i,ELUTE_ML])*100.0*CONVERSION_CONST, digits=2)
+    # formt input protein
+    inputDF$InputProtein_mg[i] <- round(as.numeric(inputDF[i,INPUT_PROTEIN_MG]), digits=2)
 }
-result <- cbind(inputDF, SPECIFIC_ACTIVITY)
-names(result) <- c("ChemProduction ID", "Input Protein (mg)", "Elute CPM", "Elute (mL)", "Comment", "Specific Activity uCi/mg" )
-write.table(result,file=params$outputPath, col.names = TRUE, sep="\t",na="", row.names=F, quote=F)
+inputDF$Specific_Activity_uCiPermg <- SPECIFIC_ACTIVITY
+
+names(inputDF) <- c("ChemProduction ID", "Input Protein (mg)", "Elute CPM", "Elute (mL)", "Comment", "Specific Activity uCi/mg" )
+write.table(inputDF,file=params$outputPath, col.names = TRUE, sep="\t",na="", row.names=F, quote=F)
