a7466d2a79fd0fec0afd0c1e06d3b5664aefdb90
mbrusnia <mbrusnia@fhcrc.org>
2019-11-13 14:00:18 -0800

    Modified to have dual modes for bat creation and run code in order to deal with dos2unix format fix.

--- src/java/org/fhcrc/optides/apps/HPLCPlateFracReporter/HPLCPlateFracReporter.java
+++ src/java/org/fhcrc/optides/apps/HPLCPlateFracReporter/HPLCPlateFracReporter.java
@@ -1,7 +1,5 @@
 package org.fhcrc.optides.apps.HPLCPlateFracReporter;
 
-import org.fhcrc.optides.apps.FastaSplit.FastaSplit;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,41 +13,69 @@ public class HPLCPlateFracReporter {
     public static void main(String[] args) {
         String plateDirectory = null; //Absolute path to the plate top directory
         String outputf = null;  //Absolute path file name of output file
-        if(args.length != 2){
+        String batf = null;
+        for(int i = 0; i < args.length; i++) {
+            //System.out.println("DEBUG number of args: " + args[i]);
+            if (args[i].split("=")[0].equals("-plateDirectory")) {
+                plateDirectory = args[i].split("=")[1];
+            } else if (args[i].split("=")[0].equals("-outputfile")) {
+                outputf = args[i].split("=")[1];
+            }
+            else if (args[i].split("=")[0].equals("-batfile")){
+                batf = args[i].split("=")[1];
+            }
+        }
+        if(plateDirectory == null || (outputf == null && batf == null)) {
+            System.out.println("USAGE: HPLCPlateFracReporter -plateDirectory=\"/Users/mbrusnia/Documents/ToolGuides/OptidesSoftware/HT_HPLC_FractionReporter\" -batfile=\"/Users/mbrusnia/Desktop/HPLCPlateFracReporter.bat\"");
+            System.out.println("Bat file should contain to run the following run after dos2unix of input files are executed");
             System.out.println("USAGE: HPLCPlateFracReporter -plateDirectory=\"/Users/mbrusnia/Documents/ToolGuides/OptidesSoftware/HT_HPLC_FractionReporter\" -outputfile=\"/Users/mbrusnia/Desktop/ReportSummary.csv\"");
             return;
         }
-        for(int i = 0; i < args.length; i++){
-            if(args[i].split("=")[0].equals("-plateDirectory")) {
-                plateDirectory = args[i].split("=")[1];
-            }
-            if(args[i].split("=")[0].equals("-outputfile"))
-                outputf = args[i].split("=")[1];
-        }
+        //System.out.println("DEBUG " +  plateDirectory + " " + outputf);
         try {
-            File fout = new File(outputf);
-            FileOutputStream fos1 = new FileOutputStream(fout);
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos1));
-            // Write out the header to reporter
-            bw.write("Vial,Sample Name,Location,Start [min],End [min],Volume [µL],tPeak selection");
-            bw.newLine();
-            HPLCPlateFracReporter report = new HPLCPlateFracReporter();
-            List<File> files = new ArrayList<File>();
-            String[] path;
-            FileReader fileReader;
-            BufferedReader br;
-            Process command;
-            report.listf(plateDirectory, files);
-            for(int i = 0;  i < files.size(); i++){
-                path = files.get(i).getName().split("/");
-                if(path[path.length-1].equals(REPORT_FILE)) {
-                    fileReader = new FileReader(files.get(i).getAbsoluteFile());
-                    br = new BufferedReader(fileReader);
-                    report.extractFractionInfo(br,bw);
-                    br.close();
+            if(batf != null) { // generating bat file for window run
+                File fout = new File(batf);
+                FileOutputStream fos1 = new FileOutputStream(fout);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos1));
+                HPLCPlateFracReporter report = new HPLCPlateFracReporter();
+                List<File> files = new ArrayList<File>();
+                report.listf(plateDirectory, files);
+                //System.out.println("DEBUG size of returned file" + files.size());
+                for (int i = 0; i < files.size(); i++) {
+                    bw.write("\"C:\\Program Files\\OptidesSoftware\\dos2unix.exe\" \"" + files.get(i).getAbsolutePath() +"\"");
+                    bw.newLine();
                 }
+                bw.write("\"C:\\Program Files\\OptidesSoftware\\HPLCPlateFracReporter.jar\" -plateDirectory=\"" + plateDirectory + "\"" + " -outputfile=\"" + outputf + "\"" );
+                bw.newLine();
+                bw.close();
             }
-            bw.close();
+            else {
+                File fout = new File(outputf);
+                FileOutputStream fos1 = new FileOutputStream(fout);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos1));
+                // Write out the header to reporter
+                bw.write("Vial,Sample Name,Location,Start [min],End [min],Volume [uL],Peak selection");
+                bw.newLine();
+                //System.out.println("DEBUG write heading to output file");
+                HPLCPlateFracReporter report = new HPLCPlateFracReporter();
+                List<File> files = new ArrayList<File>();
+                String[] path;
+                FileReader fileReader;
+                BufferedReader br;
+                Process command;
+                report.listf(plateDirectory, files);
+                //System.out.println("DEBUG size of returned file: " + files.size());
+                for (int i = 0; i < files.size(); i++) {
+                    path = files.get(i).getName().split("/");
+                    if (path[path.length - 1].equals(REPORT_FILE)) {
+                        fileReader = new FileReader(files.get(i).getAbsoluteFile());
+                        br = new BufferedReader(fileReader);
+                        report.extractFractionInfo(br, bw);
+                        br.close();
+                    }
+                }
+                bw.close();
+            }
         }catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +88,7 @@ public class HPLCPlateFracReporter {
         File[] fList = directory.listFiles();
         if(fList != null)
             for (File file : fList) {
-                if (file.isFile()) {
+                if (file.isFile() && file.getName().contains(REPORT_FILE)) {
                     files.add(file);
                 } else if (file.isDirectory()) {
                     listf(file.getAbsolutePath(), files);
